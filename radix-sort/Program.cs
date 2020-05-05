a8ade7f19cf2717129d10dc432070dc33ed37b09
fyllmz <16008117049@ogr.bozok.edu.tr>
2020-05-05 13:35:43 +0300

    first

---
+++ radix-sort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace Radix_Sort
+{
+	class Program
+	{
+		static void Sort(int[] arr)
+		{
+			int i, j;
+			int[] tmp = new int[arr.Length];
+			for (int shift = 31; shift > -1; --shift)
+			{
+				j = 0;
+				for (i = 0; i < arr.Length; ++i)
+				{
+					bool move = (arr[i] << shift) >= 0;
+					if (shift == 0 ? !move : move)
+						arr[i - j] = arr[i];
+					else
+						tmp[j++] = arr[i];
+				}
+				Array.Copy(tmp, 0, arr, arr.Length - j, j);
+			}
+		}
+		static void Main(string[] args)
+		{
+
+			int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, -67 , 51, 6, -95,6,19,22,-1};
+			Console.WriteLine("\nOrijinal Dizi : ");
+			foreach (var item in arr)
+			{
+				Console.Write(" " + item);
+			}
+
+			Sort(arr);
+			Console.WriteLine("\nSıralanmış Dizi : ");
+			foreach (var item in arr)
+			{
+				Console.Write(" " + item);
+			}
+			Console.WriteLine("\n");
+			Console.ReadLine();
+		}
+	}
+}
